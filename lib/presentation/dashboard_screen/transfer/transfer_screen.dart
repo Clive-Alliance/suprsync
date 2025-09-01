34fdb13f13b0b469a65891bcc2937360de22f5ee
Olatunji Ruth <rutherfordola@gmail.com>
2025-09-01 09:07:37 +0100

    refactor: transfer items implementation

--- lib/presentation/dashboard_screen/transfer/transfer_screen.dart
+++ lib/presentation/dashboard_screen/transfer/transfer_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart' hide ContextExtensionss;
-import 'package:searchable_paginated_dropdown/searchable_paginated_dropdown.dart';
 import 'package:suprsync/core/constants/extentions/theme_extention.dart';
 import 'package:suprsync/core/utils/app_button.dart';
 import 'package:suprsync/core/utils/show_message.dart';
+import 'package:suprsync/models/location_model.dart' as models;
+import 'package:suprsync/models/transfer_request_mdel.dart';
 import 'package:suprsync/presentation/controllers/transfer_controllers.dart';
 import 'package:suprsync/presentation/dashboard_screen/transfer/transferred_items_sheet.dart';
+import 'package:suprsync/presentation/dashboard_screen/widgets/dropdown_picker.dart';
 import 'package:suprsync/presentation/dashboard_screen/withdrawal/withdrawal_controller/withdrawal_controller.dart';
 
 class TransferScreen extends StatefulWidget {
@@ -18,7 +20,7 @@ class TransferScreen extends StatefulWidget {
 class _TransferScreenState extends State<TransferScreen> {
   final WithdrawalController _withdrawalController = Get.find();
   final TransferController _transferController = Get.find();
-  String selectedLocation = '';
+  models.LocationModel? selectedLocation; // instead of String?
 
   bool isVisible = false;
   @override
@@ -26,217 +28,133 @@ class _TransferScreenState extends State<TransferScreen> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: MediaQuery.of(context).viewInsets,
-        child: Container(
-          // color: Colors.black,
-          margin: const EdgeInsets.symmetric(vertical: 33, horizontal: 20),
-          height: size.height * 0.88,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Transferred Items List',
-                style: context.textTheme.headlineSmall?.copyWith(),
-              ),
-              const SizedBox(height: 4),
-              Text('List of transferred items from one location to another',
-                  style: context.textTheme.bodySmall
-                      ?.copyWith(color: const Color(0xff616161))),
-              const SizedBox(
-                height: 28,
-              ),
-              Text(
-                'From',
-                style: context.textTheme.bodyMedium
-                    ?.copyWith(color: const Color(0xff000000)),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Obx(() {
-                if (_withdrawalController.isLoading.value) {}
-                return SearchableDropdownFormField<int>(
-                  // margin,
-                  // searchIconWidget:
-                  //     Image.asset('assets/icons/element-4.png', height: 18),
-                  backgroundDecoration: (child) => Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Color(0xffdedede))),
-                    margin: EdgeInsets.zero,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: child,
-                    ),
+      body: Container(
+        // color: Colors.black,
+        margin: const EdgeInsets.symmetric(vertical: 33, horizontal: 20),
+        height: size.height * 0.88,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: () {
+                    _transferController.fromLocation.value = null;
+                    _transferController.toLocation.value = null;
+                    Get.back();
+                  },
+                  child: Image.asset(
+                    'assets/icons/arrow-left.png',
+                    width: 16,
+                    // height: 18,
                   ),
-                  // style: context.textTheme.bodyMedium
-                  //     ?.copyWith(color: Color(0xff848484)),
-                  hintText: Text(
-                    'Select location',
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Back',
+                  style: context.textTheme.bodySmall?.copyWith(
+                      color: const Color(0xff727272),
+                      fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 28,
+            ),
+            Text(
+              'Transferred Items List',
+              style: context.textTheme.headlineSmall?.copyWith(),
+            ),
+            const SizedBox(height: 4),
+            Text('List of transferred items from one location to another',
+                style: context.textTheme.bodySmall
+                    ?.copyWith(color: const Color(0xff616161))),
+            const SizedBox(
+              height: 28,
+            ),
+            Expanded(
+                child: SingleChildScrollView(
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                  Text(
+                    'From',
                     style: context.textTheme.bodyMedium
-                        ?.copyWith(color: Color(0xffC4C2C2)),
+                        ?.copyWith(color: const Color(0xff000000)),
                   ),
-                  margin: const EdgeInsets.all(0),
-                  dialogOffset: 05,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Obx(() {
+                    return SearchableDropdownField<models.LocationModel>(
+                      title: '',
+                      hintText: 'Select location',
+                      items: _withdrawalController.locationsModel,
+                      displayText: (location) => location.name ?? '',
+                      getValue: (location) => location.id?.toString() ?? '',
+                      selectedItem: _transferController.fromLocation.value,
+                      onChanged: (location) {
+                        _transferController.fromLocation.value = location;
 
-                  items: List.generate(
-                      _withdrawalController.locationsModel.length, (i) {
-                    var location = _withdrawalController.locationsModel[i];
-
-                    return SearchableDropdownMenuItem(
-                        value: i,
-                        label: location.name.toString(),
-                        child: StatefulBuilder(builder:
-                            (BuildContext context, StateSetter stateSetter) {
-                          return CheckboxListTile(
-                            dense: false,
-                            visualDensity: const VisualDensity(
-                                horizontal: -4, vertical: -2),
-                            contentPadding: EdgeInsets.zero,
-                            value: isVisible,
-                            onChanged: (value) {
-                              stateSetter(() {
-                                // reverse the value
-                                print(
-                                    'not sure about value${_transferController.from.value}');
-                                isVisible = !isVisible;
-                                _transferController.from.value =
-                                    location.id.toString();
-                                print(
-                                    'not sure about value 2 ${_transferController.from.value}');
-                              });
-                            },
-                            title: Text(location.name.toString(),
-                                style: context.textTheme.bodyMedium
-                                    ?.copyWith(color: const Color(0xff535353))),
-                            controlAffinity: ListTileControlAffinity
-                                .leading, // Moves the checkbox to the left
-                          );
-                        }));
+                        if (location != null) {
+                          _transferController.from.value =
+                              location.id.toString();
+                          print('Selected location: ${location.name}');
+                        }
+                      },
+                      validator: (val) =>
+                          val == null ? 'Can\'t be empty' : null,
+                    );
                   }),
-                  validator: (val) {
-                    if (val == null) return 'Can\'t be empty';
-                    print('value is not null $val');
-                    return null;
-                  },
-                  onSaved: (val) {
-                    debugPrint('On save: $val');
-                  },
-                );
-              }),
-              const SizedBox(
-                height: 18,
-              ),
-              Text(
-                'To',
-                style: context.textTheme.bodyMedium
-                    ?.copyWith(color: const Color(0xff000000)),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Obx(() {
-                if (_withdrawalController.isLoading.value) {}
-                return SearchableDropdownFormField<int>(
-                  // margin,
-                  // searchIconWidget:
-                  //     Image.asset('assets/icons/element-4.png', height: 18),
-                  backgroundDecoration: (child) => Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 1, color: Color(0xffdedede))),
-                    margin: EdgeInsets.zero,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: child,
-                    ),
+                  const SizedBox(
+                    height: 18,
                   ),
-                  // style: context.textTheme.bodyMedium
-                  //     ?.copyWith(color: Color(0xff848484)),
-                  hintText: Text(
-                    'Select location',
+                  Text(
+                    'To',
                     style: context.textTheme.bodyMedium
-                        ?.copyWith(color: Color(0xffC4C2C2)),
+                        ?.copyWith(color: const Color(0xff000000)),
                   ),
-                  margin: const EdgeInsets.all(0),
-                  dialogOffset: 05,
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Obx(() {
+                    return SearchableDropdownField<models.LocationModel>(
+                      title: '',
+                      hintText: 'Select location',
+                      items: _withdrawalController.locationsModel,
+                      displayText: (location) => location.name ?? '',
+                      getValue: (location) => location.id?.toString() ?? '',
+                      selectedItem: _transferController.toLocation.value,
+                      onChanged: (location) {
+                        _transferController.toLocation.value = location;
 
-                  items: List.generate(
-                      _withdrawalController.locationsModel.length, (i) {
-                    var location = _withdrawalController.locationsModel[i];
-
-                    return SearchableDropdownMenuItem(
-                        value: i,
-                        label: location.name.toString(),
-                        child: StatefulBuilder(builder:
-                            (BuildContext context, StateSetter stateSetter) {
-                          return CheckboxListTile(
-                            dense: false,
-                            visualDensity: const VisualDensity(
-                                horizontal: -4, vertical: -2),
-                            contentPadding: EdgeInsets.zero,
-                            value: isVisible,
-                            onChanged: (value) {
-                              stateSetter(() {
-                                // reverse the value
-                                print(
-                                    'not sure about value 3 ${_transferController.to.value}');
-                                isVisible = !isVisible;
-                                _transferController.to.value =
-                                    location.id.toString();
-                                print(
-                                    'not sure about value 4 ${_transferController.to.value}');
-                              });
-                            },
-                            title: Text(location.name.toString(),
-                                style: context.textTheme.bodyMedium
-                                    ?.copyWith(color: const Color(0xff535353))),
-                            controlAffinity: ListTileControlAffinity
-                                .leading, // Moves the checkbox to the left
-                          );
-                        }));
+                        if (location != null) {
+                          _transferController.to.value = location.id.toString();
+                          print('Selected location: ${location.name}');
+                        }
+                      },
+                      validator: (val) =>
+                          val == null ? 'Can\'t be empty' : null,
+                    );
                   }),
-                  validator: (val) {
-                    if (val == null) return 'Can\'t be empty';
-                    return null;
-                  },
-                  onSaved: (val) {
-                    debugPrint('On save: $val');
-                  },
-                );
-              }),
-              // Obx(() {
-              //   if (_withdrawalController.isLoading.value) {}
-              //   List<String> locationNames = _withdrawalController
-              //       .locationsModel
-              //       .map((location) =>
-              //           location.name ?? "Unknown") // Handle null values
-              //       .toList();
-
-              //   return CustomSearchableDropdown(
-              //     items: locationNames,
-              //     onItemSelected: (selected) {
-              //       print("Selected location: $selected");
-              //     },
-              //   );
-              // }),
-              const Expanded(
-                child: SizedBox(),
-              ),
-              RectangularButton(
-                onPress: () {
-                  if (_transferController.from.value.isEmpty) {
-                    showMessage('Please select a location', context);
-                  } else {}
-                  showTransferredItemsSheet(context);
-                },
-                buttonTitle: 'Fetch list',
-                textStyleColor: context.textTheme.labelLarge
-                    ?.copyWith(color: context.colorScheme.secondary),
-                colour: context.colorScheme.tertiary,
-                height: 50,
-              )
-            ],
-          ),
+                ]))),
+            RectangularButton(
+              onPress: () {
+                if (_transferController.from.value.isEmpty) {
+                  showMessage('Please select a location', context);
+                } else {}
+                showTransferredItemsSheet(context);
+              },
+              buttonTitle: 'Fetch list',
+              textStyleColor: context.textTheme.labelLarge
+                  ?.copyWith(color: context.colorScheme.secondary),
+              colour: context.colorScheme.tertiary,
+              height: 50,
+            )
+          ],
         ),
       ),
     );
